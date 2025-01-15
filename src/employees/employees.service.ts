2df383abfe76c67b4182eb3c7cdd1347d52f48af
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2025-01-15 14:43:44 +0530

    refactor employees module to include DatabaseService in controller and service tests

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma, Role } from '@prisma/client';
-import { DatabaseService } from 'src/database/database.service';
+import { DatabaseService } from '../database/database.service';
 
 @Injectable()
 export class EmployeesService {
