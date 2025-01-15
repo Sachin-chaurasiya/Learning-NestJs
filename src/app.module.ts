bfabd3c03dedd56677d7cca75a1ca670519761fe
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2025-01-15 15:54:51 +0530

    add PrismaClientExceptionFilter for improved error handling in global filters

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
+import { HttpStatus, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { DatabaseModule } from './database/database.module';
 import { EmployeesModule } from './employees/employees.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, HttpAdapterHost } from '@nestjs/core';
 import { MyLoggerModule } from './my-logger/my-logger.module';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
 @Module({
   imports: [
     UsersModule,
@@ -32,6 +33,18 @@ import { MyLoggerModule } from './my-logger/my-logger.module';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_FILTER,
+      useFactory: ({ httpAdapter }: HttpAdapterHost) => {
+        return new PrismaClientExceptionFilter(httpAdapter, {
+          // Prisma Error Code: HTTP Status Response
+          P2000: HttpStatus.BAD_REQUEST,
+          P2002: HttpStatus.CONFLICT,
+          P2025: HttpStatus.NOT_FOUND,
+        });
+      },
+      inject: [HttpAdapterHost],
     },
   ],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './all-exceptions.filter';
+// import { AllExceptionsFilter } from './all-exceptions.filter';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+import { HttpStatus } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const { httpAdapter } = app.get(HttpAdapterHost);
-  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  // app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+  app.useGlobalFilters(
+    new PrismaClientExceptionFilter(httpAdapter, {
+      // Prisma Error Code: HTTP Status Response
+      P2000: HttpStatus.BAD_REQUEST,
+      P2002: HttpStatus.CONFLICT,
+      P2025: HttpStatus.NOT_FOUND,
+    }),
+  );
 
   app.enableCors();
   app.setGlobalPrefix('api');
